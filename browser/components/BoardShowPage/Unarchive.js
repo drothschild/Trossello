a60a6ec6f28d110db0884fb40d7b9df96e1acdf4
David Rothschild <david@rothschildsolutions.com>
2016-11-14 17:39:34 -0800

    add cards to unarchive

--- browser/components/BoardShowPage/Unarchive.js
+++ browser/components/BoardShowPage/Unarchive.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react'
 import boardStore from '../../stores/boardStore'
 import $ from 'jquery'
 
-export default class ChangeBackground extends Component {
+export default class Unarchive extends Component {
   static PropTypes = {
     board: React.PropTypes.object.isRequired
+  }
+  render(){
+    const { board } = this.props
+    const cards = board.cards
+      .filter(card => card.archived)
+      .sort((a, b) => a.order - b.order)
   }
 }
