34c9a13cbc67f0ae78a3953ba164983348619991
David Rothschild <david@rothschildsolutions.com>
2016-11-18 13:17:57 -0800

    add select list of board lists.

--- browser/components/BoardShowPage/CopyCard.js
+++ browser/components/BoardShowPage/CopyCard.js
@@ -7,7 +7,7 @@ import Button from '../Button'
 import ToggleComponent from '../ToggleComponent'
 import ConfirmationButton from '../ConfirmationButton'
 import boardStore from '../../stores/boardStore'
-import '../ModalView'
+import DialogBox from '../DialogBox'
 import $ from 'jquery'
 
 
@@ -16,6 +16,8 @@ export default class CopyCard extends Component {
     card: React.PropTypes.object.isRequired,
     list: React.PropTypes.object.isRequired,
     board: React.PropTypes.object.isRequired,
+    top: React.PropTypes.integer.isRequired,
+    left: React.PropTypes.integer.isRequired,
   }
   constructor(props){
     super(props)
@@ -28,5 +30,24 @@ export default class CopyCard extends Component {
   render() {
 
     return <DialogBox />
+  }
+}
+
+class ListsMenu extends Component {
+  static propTypes = {
+    list: React.PropTypes.object.isRequired,
+    board: React.PropTypes.object.isRequired,
+  }
+
+  render() {
+    const lists = board.lists
+    .filter(list => !list.archived)
+    .map( list => {
+         <option value={list.id}>{list.name}</option>
+       }).toString()
+    return 
+      <select className="CopyCard-list">
+        {lists}
+      </select>
   }
 }
